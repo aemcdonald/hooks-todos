6a5ba230cd8f78f190830909d351eb27235a5cbf
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-29 23:47:47 -0700

    Add functionality to edit todo

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,29 +1,37 @@
-import React, { useState, useContext, useEffect } from 'react'
-import TodosContext from '../context'
+import React, { useState, useContext, useEffect } from 'react';
+import TodosContext from '../context';
 
 export default function TodoForm() {
-    const [todo, setTodo] = useState('')  
+  const [todo, setTodo] = useState('');
 
-    const { state: { currentTodo = {} }, dispatch } = useContext(TodosContext)
+  const {
+    state: { currentTodo = {} },
+    dispatch
+  } = useContext(TodosContext);
 
-    useEffect(() => {
-        if(currentTodo.text) {
-            setTodo(currentTodo.text)
-        }
-    }, [currentTodo.id])
-
-    const handleSubmit = event => {
-        event.preventDefault()
-        dispatch({ type: 'ADD_TODO', payload: todo })
-        setTodo('')
+  useEffect(() => {
+    if (currentTodo.text) {
+      setTodo(currentTodo.text);
     }
-    return(
-        <form onSubmit={handleSubmit}>
-            <input
-                type='text'
-                onChange={event => setTodo(event.target.value)}
-                value={todo}
-            />
-        </form>
-    )
+  }, [currentTodo.id]);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (currentTodo.text) {
+      dispatch({ type: 'UPDATE_TODO', payload: todo });
+    } else {
+      dispatch({ type: 'ADD_TODO', payload: todo });
+    }
+    setTodo('');
+  };
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        onChange={event => setTodo(event.target.value)}
+        value={todo}
+      />
+    </form>
+  );
 }
