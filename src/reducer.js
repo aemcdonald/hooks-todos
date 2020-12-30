7b8c372a493278f3229689d574355dd7b4e102aa
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-29 22:53:25 -0700

    Add toggle todo case

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,15 @@
 function reducer(state, action) {
   switch (action.type) {
+    case 'TOGGLE_TODO':
+      const toggledTodos = state.todos.map(todo =>
+        todo.id === action.payload.id
+          ? { ...action.payload, coplete: !action.payload.complete }
+          : todo
+      );
+      return {
+        ...state,
+        todos: toggledTodos
+      };
     default:
       return state;
   }
