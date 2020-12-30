fe39da5076e5bf0b945c830e4d1f253b24953e2d
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-29 21:35:25 -0700

    Create context

--- src/context.js
+++ src/context.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const TodosContext = React.createContext({});
+const TodosContext = React.createContext({
+  todos: [
+    { id: 1, text: 'Make grocery list', complete: true },
+    { id: 2, text: 'Do laundry', complete: false },
+    { id: 3, text: 'Finish project', complete: true }
+  ]
+});
 
 export default TodosContext;
