5fb34d08d86b0fab5b859a65f37f6ddbcd88f910
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-29 23:14:54 -0700

    Create TodoForm component

---
+++ src/components/TodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TodoForm() {
+    return(
+        <form>
+            <input
+                type='text'
+            />
+        </form>
+    )
+}
