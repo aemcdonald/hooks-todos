ea6026dc00e77ee302ccbe83cc467c34b2ff75d0
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-12-29 21:53:37 -0700

    Map over todos and return

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,5 +2,17 @@ import React, { useContext } from 'react';
 import TodosContext from '../context';
 
 export const TodoList = () => {
-    const { state, dispatch } = useContext(TodosContext)
-}
+  const { state, dispatch } = useContext(TodosContext);
+
+  return (
+    <div>
+      <ul>
+        state.todos.map(todo => {
+          <li key={todo.id}>
+            <span>{todo.text}</span>
+          </li>
+        })
+      </ul>
+    </div>
+  );
+};
